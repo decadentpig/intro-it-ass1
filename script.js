d4a62c323ad1e4cd23e97414bb69f47f353360a7
Jordan De Visser <jordandevisser@gmail.com>
2021-03-07 15:33:01 +1000

    Added body content, JS section toggles, image fixed to background

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-function toggleSection(selection){
-    //let paragraph = document.getElementById(selection);
-    //paragraph.style.display = grid;
-    //paragraph.innerText = 'test string';
+function toggleSection(section){
+  let para = document.getElementById(section);
+  let sectionsArray = [
+    'para-info',
+    'para-interest',
+    'para-job',
+    'para-profiles', 
+    'para-project'
+  ];
+  let newArray = [];
+
+  switch (para.style.display){
+    case 'grid':
+      para.style.display = 'none';
+      break;
+    case 'none':
+    default:
+      para.style.display = 'grid';
   }
+
+  sectionsArray.forEach(element => {
+    if (element != section) newArray.push(element);
+  })
+  newArray.forEach(element => {
+    document.getElementById(element).style.display = 'none';
+  })
+}
